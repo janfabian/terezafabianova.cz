8b8d494cbb6b87ae4d16a926c91db9d5bf39d266
janfabian <jan.fabian@topmonks.com>
2017-10-14 00:53:06 +0300

    Mail form

--- web/js/index.js
+++ web/js/index.js
@@ -3,3 +3,28 @@ function gen_mail_to_link(lhs, rhs, subject) {
     document.write(":" + lhs + "@");
     document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/a>");
 }
+
+function onContactSubmit(data) {
+  var $form = $("#contact-form");
+  var formData = {};
+  $form.find("[name]").each(function(i, el){
+    var $el = $(el);
+    formData[$el.attr("name")] = $el.val();
+  });
+  $.ajax({
+    type: "POST",
+    url: "https://api.terezafabianova.cz/sendEmail",
+    data: JSON.stringify(formData),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8"
+  })
+  .done(function(data) {
+    console.log(data);
+  })
+  .error(function(error) {
+    alert(JSON.stringify(error));
+  })
+  .always(function() {
+    grecaptcha.reset()
+  });
+};
